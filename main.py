af443f78dbc223259d8351bf1a0473ecc8c16672
dcmorris05 <dcmorris2205@gmail.com>
2026-04-08 02:03:49 -0700

    Update main.py
    defined all functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_domain(mapping: dict) -> set:
     """Return the domain X (all inputs of the function)."""
     # === TODO ===
     # Your code here
-    pass
+    return set(mapping.keys())
     # === END TODO ===
 
 
@@ -13,7 +13,7 @@ def get_range(mapping: dict) -> set:
     """Return the range — the set of outputs actually mapped to."""
     # === TODO ===
     # Your code here
-    pass
+    return set(mapping.values())
     # === END TODO ===
 
 
@@ -21,7 +21,8 @@ def is_well_defined(mapping: dict, target: set) -> bool:
     """Return True if every output value is in the target set."""
     # === TODO ===
     # Your code here
-    pass
+    #if range of mapping is subset of target return rtue
+    return True if get_range(mapping) <= target else False
     # === END TODO ===
 
 
@@ -29,7 +30,9 @@ def is_injective(mapping: dict) -> bool:
     """Return True if f is one-to-one (no two inputs map to same output)."""
     # === TODO ===
     # Your code here
-    pass
+    #if any output is duplicated itll be removed when get_rage produces
+    #the outputd as a set, which would make the len shorter
+    return True if len(get_range(mapping)) == len(mapping) else False
     # === END TODO ===
 
 
@@ -37,7 +40,7 @@ def is_surjective(mapping: dict, target: set) -> bool:
     """Return True if f is onto (range == target)."""
     # === TODO ===
     # Your code here
-    pass
+    return True if get_range(mapping) == target else False
     # === END TODO ===
 
 
@@ -45,5 +48,5 @@ def is_bijective(mapping: dict, target: set) -> bool:
     """Return True if f is both injective and surjective."""
     # === TODO ===
     # Your code here
-    pass
+    return True if is_surjective(mapping, target) and is_injective(mapping) else False
     # === END TODO ===
